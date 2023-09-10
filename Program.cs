815ef96161c68f6083443ae49ad27c0e2d1280b1
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-09-10 20:51:05 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using PhoneNumbersLib;
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Title = "PhoneNumberInfo | Made by https://github.com/GabryB03/";
+
+        while (true)
+        {
+            string phoneNumber = "";
+
+            while (!PhoneNumbers.IsPhoneNumberValid(phoneNumber))
+            {
+                Console.Write("Please, enter a valid phone number: ");
+                phoneNumber = Console.ReadLine();
+
+                if (!PhoneNumbers.IsPhoneNumberValid(phoneNumber))
+                {
+                    Console.WriteLine("Invalid phone number, please try again.");
+                }
+            }
+
+            PhoneNumber informations = new PhoneNumber(phoneNumber);
+
+            Console.WriteLine($"\r\n" +
+                $"INFORMATIONS" +
+                $"\r\n\r\n" +
+                $"Complete phone number: {informations.ToString()}\r\n" +
+                $"Prefix: +{informations.Prefix}\r\n" +
+                $"Number: {informations.Number}\r\n" +
+                $"Country: {informations.Country}\r\n" +
+                $"Country code: {informations.CountryCode}\r\n" +
+                $"Country with code: {informations.Country} ({informations.CountryCode})\r\n" +
+                (informations.Country == "Italy" ? $"Possible italian operator: {informations.GetPossibleItalianOperator()}\r\n" : ""));
+        }
+    }
+}
